bf4796dca95eb3aba6ad8d537084708c9a04b9dd
victor <viktor.orkush@gmail.com>
2018-10-24 19:08:52 +0300

    add views for Groups: JoinGroup and LeaveGroup add to they url

--- groups/views.py
+++ groups/views.py
@@ -1,9 +1,13 @@
+from django.db import IntegrityError
 from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.contrib import messages
 
 from django.views import generic
+
+from groups import models
 from groups.models import Group, GroupMember
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
@@ -15,3 +19,38 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
+
+class JoinGroup(LoginRequiredMixin,generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reversed('group:simple', kwargs={'simple':self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_400(Group, slug=self.kwargs.get('slug'))
+
+        try:
+            GroupMember.objects.create(user=self.request.user, group=group)
+        except IntegrityError:
+            messages.warning(self.request, 'Warning already amember!')
+        else:
+            messages.warning(self.request, 'You are now a member!')
+
+        return super().get(request,*args,**kwargs)
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reversed('group:simple', kwargs={'simple':self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+            membership = models.GroupMember.objects.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get('slug')
+            ).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, 'Sorry you are not in this group!')
+        else:
+            membership.delete()
+            messages.success(self.request, 'You have left the group')
+        return super().get(request,*args,**kwargs)
